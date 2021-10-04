85a92289f57babdb2a0557c41d4572e1e192d2c1
unknown <ninadmsn@outlook.com>
2021-10-04 11:59:30 +0530

    git ignore added

--- sensor_analysis/sensor_analysis/settings.py
+++ sensor_analysis/sensor_analysis/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sensor_analysis',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'MySQL@10',
         'HOST':'localhost',
         'PORT':'3306',
     }
