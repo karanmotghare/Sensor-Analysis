94a0207bff225f237e5a250c0dbcacc41ad3bab5
Aryan <aryan@Aryans-MacBook-Pro.local>
2021-10-12 22:15:35 +0530

    Selection List

--- sensor_analysis/static/js/javascript.js
+++ sensor_analysis/static/js/javascript.js
@@ -44,7 +44,7 @@ function change_location(){
         success: function(sg){
             let html_data = '<option value="" disabled selected>Select Sensor group</option>';
             sg.forEach(function (sg) {
-                html_data += `<option value="${sg.sg_id}">${sg.sg_name}</option>`
+                html_data += `<option id="sgrp_id" value="${sg.sg_id}">${sg.sg_name}</option>`
             });
 
             var sengrp = document.getElementById('sngrp_list');
@@ -73,11 +73,96 @@ function change_sg(){
         success: function(sensor){
             let html_data = '<option value="" disabled selected>Select Sensor</option>';
             sensor.forEach(function (sensor) {
-                html_data += `<option value="${sensor.sensor_id}">${sensor.sensor_name}</option>`
+                html_data += `<option id="sns_id" value="${sensor.sensor_id}">${sensor.sensor_name}</option>`
             });
 
             var sen = document.getElementById('sns_list');
             sen.innerHTML = html_data;
         }
     });
+}
+
+// Function to add sensor to compare on homepage
+function already_exists(val){
+    var arr = document.getElementById('selection').options;
+    
+    val = JSON.stringify(val.join());
+
+    for(var i=0; i<arr.length; i++)
+    {
+        var op = JSON.stringify(arr[i].value);
+        
+        if(op == val)
+        {
+            // Already exists
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function add_to_compare(){
+    var loc = document.getElementById('location_list');
+    var loc_id = loc.value;
+    var loc_name = loc.options[loc.selectedIndex].text;
+
+    var sg = document.getElementById('sngrp_list');
+    var sg_id = sg.value;
+    var sg_name = sg.options[sg.selectedIndex].text;
+
+    var sensor = document.getElementById('sns_list');
+    var sensor_id = sensor.value;
+    var sensor_name = sensor.options[sensor.selectedIndex].text;
+
+    var val = new Array(loc_id, sg_id, sensor_id);
+
+    var present = already_exists(val);
+
+    console.log(val);
+
+    if(!loc_id || !sg_id || !sensor_id)
+    {
+        console.log("Invalid sensor selection");
+        var err = document.getElementById('err_msg');
+        err.innerHTML = '<p>Invalid Selection !</p>';
+    }
+    else if(present)
+    {
+        console.log("Already present");
+        var err = document.getElementById('err_msg');
+        err.innerHTML = '<p>Selection Already Exists !</p>';
+    }
+    else 
+    {
+        console.log("Valid Selection");
+        var err = document.getElementById('err_msg');
+        err.innerHTML = '';
+
+        selected = document.getElementById('selection');
+        selected_data = selected.innerHTML;
+        selected_data += `<option value="${val}">${loc_name} / ${sg_name} / ${sensor_name}</option>`;
+        selected.innerHTML = selected_data;
+    }
+}
+
+// Function to remove sensor from selected list
+function remove_from_list(){
+
+    var opt = document.getElementById('selection');
+    var is_selected = [];
+
+    for(var i=0; i<opt.options.length; i++)
+    {
+        is_selected[i] = opt.options[i].selected;
+    }
+
+    // Remove from bottom up
+    for(var i=opt.options.length-1; i>=0; i--)
+    {
+        if(opt.options[i].selected)
+        {
+            opt.remove(i);
+        }
+    }
 }
